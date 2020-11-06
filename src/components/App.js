e70641fd22ca2e16a66ff2837d48e80ee5de8b87
Brandon Albright <brandon4692@gmail.com>
2020-11-05 20:29:15 -0600

    day 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Header from './Header'
+import Login from './Login'
+import Posts from './Posts'
+import Message from './Message'
+import CreateNewMessage from './CreateNewMessage'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="posts">
+            <Posts />
+          </Route>
+          <Route path="/posts">
+            <Posts />
+          </Route>
+          <Route path="/createNewmessage">
+            <CreateNewMessage />
+          </Route>
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
