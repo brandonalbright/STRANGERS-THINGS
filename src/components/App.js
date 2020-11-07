ab9fff750b667b02096a0177b586ee159e9c8ba6
fakhan <amank9008@gmail.com>
2020-11-07 10:53:14 -0600

    login code

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Header from './Header'
 import Login from './Login'
 import Posts from './Posts'
 import Message from './Message'
 import CreateNewMessage from './CreateNewMessage'
+import { getToken } from '../api/index'
+import { hitAPI } from '../api/index'
 
 function App() {
+  // a piece of state that represents the status of the current user
+  const [isLoggedIn, setIsLoggedIn] = useState(!!getToken())
+  const [postList, setPostList] = useState([])
+
+  useEffect(() => {
+    hitAPI('GET', '/posts')
+      .then((data) => {
+        const { posts } = data
+        setPostList(posts)
+      })
+      .catch(console.error)
+  }, [isLoggedIn])
+
   return (
     <Router>
       <div>
         <Switch>
           <Route path="/login">
-            <Header />
-            <Login />
+            <Header isLoggedIn={isLoggedIn} />
+            <Login isLoggedIn={isLoggedIn} />
           </Route>
           <Route path="posts">
             <Header />
