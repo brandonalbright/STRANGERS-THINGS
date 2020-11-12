75a91779f42a85dec7a7e8d898cd41651c4e1947
fakhan <amank9008@gmail.com>
2020-11-11 21:04:40 -0600

    adding message

--- src/components/CreateNewMessage.js
+++ src/components/CreateNewMessage.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react'
 import './CreateNewMessage.css'
+import { hitAPI } from '../api'
 
-function CreateNewMessage() {
+function CreateNewMessage(props) {
+  const { postId } = props
+
   const [textValue, setTextValue] = useState('')
+
   return (
-    <div className="new-message">
-      <form>
-        <textarea value={textValue} onChnage={() => {}}></textarea>
+    <div id="message-form" className="new-message">
+      <form
+        onSubmit={async (event, post) => {
+          event.preventDefault()
+          const objBody = {
+            message: {
+              content: textValue,
+            },
+          }
+          const result = await hitAPI(
+            'POST',
+            `/posts/${postId}/messages`,
+            objBody,
+          )
+          setTextValue('')
+          document.getElementById('message-form').style.display = 'none'
+        }}
+      >
+        <textarea
+          value={textValue}
+          onChange={(event) => setTextValue(event.target.value)}
+        ></textarea>
         <button>Send</button>
       </form>
     </div>
