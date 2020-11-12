502f934d4ce92bb2fe3daeda4155d1aa40d49d7f
Brandon Albright <brandon4692@gmail.com>
2020-11-11 21:00:38 -0600

    add myposts

---
+++ src/components/MyPosts.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import Posts from './Posts'
+
+function MyPosts(props) {
+    const {postList} = props;
+    
+
+    function filteredMyPosts() {
+        filteredMyPosts = postList.filter((post) => {
+          return post.isAuthor;
+        })
+        return filteredMyPosts
+      }
+
+    return (
+        <>
+        <Posts postList={filteredMyPosts()} />
+        </>
+    )
+}
+
+export default MyPosts
